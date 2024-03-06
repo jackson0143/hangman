c6b28f33e3444b6f656ebaae75ad2be8aec379e7
jackson <jngu0076@student.monash.edu>
2024-03-07 01:21:18 +1100

    update to game.py

--- game.py
+++ game.py
@@ -1,1 +1,19 @@
-failed = 0
+from csv_reader import csv_reader
+
+def display_main_menu():                                                                #main menu printing
+    print("--- Main Menu ---")                                  
+    print("1. Play game")
+    print("2. Exit")
+
+def main():
+    print("WELCOME TO THE MINI-HANGMAN GAME")
+    print("")
+
+    game_count = 0
+    print(f"Your total game sessions:{game_count}")
+
+    display_main_menu()
+    choice = int_input("Please choose an option: ", restricted_to=[1,2])
+    print("")
+    if choice == 1:
+        Round()
--- round.py
+++ round.py
@@ -1,16 +1,29 @@
 
 from csv_reader import csv_reader
 import random
-class Round:
-    def __init__(self, dictionary) -> None:
-        
-        self.dictionary = dictionary
+
+def single_char_input(prompt=""):
+    """
+    Function to request a single input
+    """
+    while True:			
+        try:
+            user_input = input(prompt)                                              #requests input from user
+
+            while len(user_input) != 1 or not user_input.isalpha():                 #keeps prompting if invalid input
+                user_input = input("Please enter a valid single character: ")
+        except ValueError:
+            continue
+        else:
+            break
+    return user_input
+
+
+
+
+def Round(dictionary):
+
         secret_word = random.choice(dictionary)
-        
-
-
-        
-
         turns = 7
         guess = ''
         guess_display = ""
@@ -18,34 +31,50 @@ class Round:
             if char != " ":
                 guess_display += "_"
             else:
-                guess_display +=" "
+                guess_display += " "
 
+    
         print(guess_display)
 
-
-        
+        guess_history = []
         while turns > 0:    
-            print("Guess a chracter: ")        
-            guess = input()
-            if guess in secret_word:
-                guess_display = list(guess_display)
-                for i in range(len(secret_word)):
-                    if guess == secret_word[i]:
-                        guess_display[i] = guess
-                guess_display = "".join(guess_display)
+           
+    
+
+            print(f"guess history:{guess_history}")   
+            guess = single_char_input("Guess a chracter: ")
+            print("\n")
+            if guess not in guess_history:
+                guess_history.append(guess)
+                if guess in secret_word:
 
 
+
+                    guess_display = list(guess_display)
+                    for i in range(len(secret_word)):
+                        if guess == secret_word[i]:
+                            guess_display[i] = guess
+                    guess_display = "".join(guess_display)
+
+
+
+
+                else:
+                    turns -= 1
+                    print("WRONG!")
+                    print(f"you have {turns} turns left")
             else:
-                turns -= 1
-                print(f"you have {turns} turns left")
+                print("You have already guessed this")
+                print(f"guess history:{guess_history}")
 
 
             #check if you guessed all the characters
             if '_' not in guess_display:
                 break
 
+            print("***************************************")
             print(guess_display)
-
+            
 
         if turns <=0:
             print("You lose")
@@ -53,7 +82,3 @@ class Round:
         else:
             print("You win")
    
-
-dictionary = csv_reader("fruits.csv")
-print(dictionary)
-test = Round(dictionary)
